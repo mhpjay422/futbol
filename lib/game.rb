8ec90487bb1b35a13eff5057ba9b135a2db25c8d
Jason Gong <jasongong83@gmail.com>
2021-04-08 16:12:18 -0400

    convert data to desired format in initialize. return array of Game objects from Game::load_games

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,14 @@ class Game
   attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id,:away_goals, :home_goals, :venue, :venue_link
 
   def initialize(data)
-    @game_id = data[0]
-    @season = data[1]
+    @game_id = data[0].to_i
+    @season = [data[1][0..3].to_i,data[1][4..7].to_i]
     @type = data[2]
-    @date_time = data[3]
-    @away_team_id = data[4]
-    @home_team_id = data[5]
-    @away_goals = data[6]
-    @home_goals = data[7]
+    @date_time = Date.strptime(data[3], "%m/%d/%Y")
+    @away_team_id = data[4].to_i
+    @home_team_id = data[5].to_i
+    @away_goals = data[6].to_i
+    @home_goals = data[7].to_i
     @venue = data[8]
     @venue_link = data[9]
   end
@@ -19,5 +19,6 @@ class Game
     CSV.foreach(data, headers:true, header_converters: :symbol) do |row|
       arr << Game.new(row)
     end
+    arr
   end
 end 
