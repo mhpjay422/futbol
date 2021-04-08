199ca7ef90b883a6d0bd76689dcada344b33be8f
Jason Gong <jasongong83@gmail.com>
2021-04-08 17:58:29 -0400

    add average_goals_by_season method to stattracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -46,16 +46,16 @@ class StatTracker
   
   def percentage_home_wins(games)
     home_wins = games.reduce(0) {|total, game| total += 1 if game.home_goals > game.away_goals; total }
-    home_wins / games.length.to_f * 100
+    home_wins / games.length.to_f * 100.round(2)
   end
   
   def percentage_visitor_wins(games)
     visitor_wins = games.reduce(0) {|total, game| total += 1 if game.away_goals > game.home_goals; total }
-    visitor_wins / games.length.to_f * 100
+    visitor_wins / games.length.to_f * 100.round(2)
   end
   
   def percentage_ties(home_percentage, away_percentage)
-    100 - (home_percentage + away_percentage).to_f
+    100 - (home_percentage + away_percentage).to_f.round(2)
   end
   
   def count_of_games_by_season(games)
@@ -72,10 +72,25 @@ class StatTracker
   
   def average_goals_per_game(games)
     total_goals = games.reduce(0) {|total, game| total += (game.away_goals + game.home_goals); total}
-    total_goals / games.length.to_f
+    total_goals / games.length.to_f.round(2)
   end
   
   def average_goals_by_season(games)
-  
+    hash = {}
+    games.each do |game|
+      season = game.season
+      home_goals = game.home_goals
+      away_goals = game.away_goals
+
+      if hash[season]
+        hash[season][0] += 1
+        hash[season][1] += (home_goals + away_goals)
+      else 
+        hash[season] = [1, (home_goals + away_goals)]
+      end
+    end
+    hash = hash.each {|game| 
+    # binding.pry;
+     hash[game[0]] = game[1][1] / game[1][0].to_f.round(2)}
   end
 end
--- runner.rb
+++ runner.rb
@@ -14,11 +14,11 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 all_games = stat_tracker.game_collection
-highest_combined_score = stat_tracker.highest_total_score(all_games)
-lowest_combined_score = stat_tracker.lowest_total_score(all_games)
-percent_home_victory = stat_tracker.percentage_home_wins(all_games)
-percent_visitor_victory = stat_tracker.percentage_visitor_wins(all_games)
-percent_score_even = stat_tracker.percentage_ties(percent_home_victory, percent_visitor_victory)
-num_games_per_season = stat_tracker.count_of_games_by_season(all_games)
-goals_average = stat_tracker.average_goals_per_game(all_games)
-puts goals_average
+# puts highest_combined_score = stat_tracker.highest_total_score(all_games)
+# puts lowest_combined_score = stat_tracker.lowest_total_score(all_games)
+# puts percent_home_victory = stat_tracker.percentage_home_wins(all_games)
+# puts percent_visitor_victory = stat_tracker.percentage_visitor_wins(all_games)
+# puts percent_score_even = stat_tracker.percentage_ties(percent_home_victory, percent_visitor_victory)
+# puts num_games_per_season = stat_tracker.count_of_games_by_season(all_games)
+# puts avg_goals_game = stat_tracker.average_goals_per_game(all_games)
+puts avg_goals_season = stat_tracker.average_goals_by_season(all_games)
