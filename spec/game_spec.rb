ae14e0cc956f803f08b3e5efa92179bb86fb9aa3
Jason Gong <jasongong83@gmail.com>
2021-04-11 15:58:45 -0400

    add test for Game::new object initialization

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,28 @@
 require '../lib/stat_tracker.rb'
 require '../lib/game.rb'
 require 'pry'
+require 'csv'
 
 describe Game do 
+
+
+
+  describe 'new' do 
+    before :each do 
+      @csvrow = nil
+      CSV.foreach('../data/games.csv', headers:true, header_converters: :symbol) do |row|
+        @csvrow = row
+        break
+      end 
+    end
+
+      context 'given a row of csv data' do 
+        it 'returns a Game object' do 
+
+          expect(Game.new(@csvrow).class).to eq(Game)
+        end
+    end
+  end
 
   describe '::load_games' do 
     context 'given a file path to the csv data' do 
