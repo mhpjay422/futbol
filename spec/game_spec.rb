0c96e97dc6b9316b5c6740708937daffb30b630b
Jason Gong <jasongong83@gmail.com>
2021-04-11 16:45:51 -0400

    add test for Game::load_games to test that the array is filled with Game objects

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,9 +21,10 @@ describe Game do
 
   describe '::load_games' do 
     context 'given a file path to the csv data' do 
-      it 'returns an array of game objects' do 
+      it 'returns an array of Game objects' do 
 
         expect(Game.load_games('../data/games.csv').class).to eq(Array)
+        expect(Game.load_games('../data/games.csv').all? {|game| game.class == Game}).to eq(true)
       end
     end
   end
