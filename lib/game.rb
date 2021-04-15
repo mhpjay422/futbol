adab29824a4332f223ef76acb47597cc6a2d4953
Jason Gong <jasongong83@gmail.com>
2021-04-15 11:53:49 -0400

    add where method to that takes names of methods as params to call other Game methods to query data

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,14 @@ class Game
   extend Finder
   extend DataLoader
 
-  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id,:away_goals, :home_goals, :venue, :venue_link
+  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
 
   def initialize(data)
     @game_id = data[0].to_i
     @season = data[1].to_i
     @type = data[2]
-    @date_time = Date.strptime(data[3], "%m/%d/%Y")
+    @date_time = data[3]
+    # @date_time = Date.strptime(data[3], "%m/%d/%Y")
     @away_team_id = data[4].to_i
     @home_team_id = data[5].to_i
     @away_goals = data[6].to_i
@@ -18,11 +19,47 @@ class Game
   end
 
   def self.sort_by_total_score
-    sorted = Game.all.sort_by {|game| game.home_goals + game.away_goals}
+    Game.all.sort_by {|game| game.home_goals + game.away_goals}
   end
+
 
   def total_score 
     home_goals + away_goals
   end
+
+  def winner 
+    
+    # binding.pry
+    
+    return nil if home_goals == away_goals
+    return self.home_team_id if home_goals > away_goals
+    return self.away_team_id if away_goals > home_goals
+  end
+
+  def loser
+    return nil if home_goals == away_goals
+    return away_team if away_goals < home_goals
+    return home_team if home_goals < away_goals
+  end
+
+  def away_team  
+    self.away_team_id
+  end
+
+  def home_team 
+    # binding.pry;
+    self.home_team_id
+  end
+
+  def self.where(objective, target)
+    Game.all.select do |game| 
+       target == :nil ? game.send(objective) == nil : game.send(objective) == game.send(target)
+       # send is a method that invokes its param (which is the name of a method)
+       # target eval to nil is testing if the game result ended in a tie
+       # if so, finds games where result is nil ( tie )
+       # otherwise eval calls objective and target and brings back results that are equal
+    end
+  end
+
 
 end 
